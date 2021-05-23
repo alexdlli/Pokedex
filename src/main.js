13ef57252943a99de9a3b5967ea8f1a2189aed5d
Alex Sandro <alexsandrojr.303@gmail.com>
2021-05-23 14:34:49 -0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue';
+import InfiniteLoading from 'vue-infinite-loading';
+
+import './styles/main.scss';
+
 import App from './App.vue';
 
 Vue.config.productionTip = false;
 
-new Vue({
-  render: (h) => h(App),
-}).$mount('#app');
+Vue.use(InfiniteLoading, {});
+
+new Vue({	render: h => h(App) }).$mount('#root');
